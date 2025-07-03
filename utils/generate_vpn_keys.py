e90a30581ceec93de2df6f3030dda5a6e4d334fe
macbookair <cr7dima777@gmail.com>
2025-07-03 22:55:29 +0300

    v1.0.2

--- utils/generate_vpn_keys.py
+++ utils/generate_vpn_keys.py
@@ -7,8 +7,6 @@ import paramiko
 import qrcode
 import secrets
 import random
-from telebot.types import Message
-from urllib.parse import quote
 from cryptography.hazmat.primitives.asymmetric import x25519
 from cryptography.hazmat.primitives import serialization
 
@@ -346,7 +344,7 @@ def setup_server(server_obj: Server) -> bool:
         return False
 
 
-def generate_key(server_obj: Server, message: Message) -> VPNKey | None:
+def generate_key(server_obj: Server) -> VPNKey | None:
     """
     Генерирует новый VPN ключ для сервера.
 
@@ -416,9 +414,9 @@ def generate_key(server_obj: Server, message: Message) -> VPNKey | None:
             app_logger.error(f"Ошибка при парсинге конфигурационного файла: {e}")
             return None
 
-        full_name = message.from_user.full_name
-        full_name_encoded = quote(full_name)
-
+        random_number = random.randint(1, 999)
+        key_number = f"{random_number:03}"
+        name = f"{server_obj.location} #{key_number}"
         vless_link = (
             f"vless://{client_uuid}@{server_obj.ip_address}:443?"
             f"security=reality&"
@@ -428,7 +426,7 @@ def generate_key(server_obj: Server, message: Message) -> VPNKey | None:
             f"fp={XRAY_REALITY_FINGERPRINT}&"
             f"sni={server_name}&"
             f"pbk={public_key}&"
-            f"sid={short_id}#{full_name}"
+            f"sid={short_id}#xDoubleVPN:{name}"
         )
         app_logger.info(f"Сформирована VLESS ссылка: {vless_link}")
 
